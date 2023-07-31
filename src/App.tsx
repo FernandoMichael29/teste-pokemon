6e6c9aa8fb6c4d135c73ac78bdbfd8c303c9ff48
Fernando Michael <bigmaicon1@hotmail.com>
2023-07-31 20:04:00 -0300

    starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
+  <>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +18,6 @@ function App() {
         </a>
       </header>
     </div>
+  </>
   );
-}
-
-export default App;
+};
