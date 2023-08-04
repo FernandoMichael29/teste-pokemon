14c606c2f1829a4cfc39e5885591e8f6b12452df
Fernando Michael <bigmaicon1@hotmail.com>
2023-08-04 17:52:43 -0300

    adicionando mensagem em toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ToastContainer } from 'react-toastify';
 import { GlobalStyle } from './css';
 import { Rotas } from './routes';
 import { Theme } from './themes';
@@ -15,6 +16,7 @@ export const App = () => {
         />
       </Helmet>
       <GlobalStyle />
+      <ToastContainer position="top-right" closeOnClick pauseOnHover />
       <Rotas />
     </Theme>
   </>
