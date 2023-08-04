639e0b557511c0dbe0fad047cca40375b5fc707b
Fernando Michael <bigmaicon1@hotmail.com>
2023-08-04 17:51:47 -0300

    importando css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { GlobalStyle } from './css';
 import { Rotas } from './routes';
 import { Theme } from './themes';
+import { Helmet } from "react-helmet";
+import 'react-toastify/dist/ReactToastify.css';
 
 export const App = () => {
   return (
   <>
     <Theme>
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&family=Ubuntu:wght@700&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <GlobalStyle />
       <Rotas />
     </Theme>
