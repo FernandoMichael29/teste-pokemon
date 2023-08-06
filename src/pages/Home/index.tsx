b1475b16473813005d0981eac0f42a3d432499fb
Fernando Michael <bigmaicon1@hotmail.com>
2023-08-06 16:15:16 -0300

    requisição para chamar mais pokemons no final da página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,8 +3,8 @@ import { Body } from "../../component/atoms/body";
 import axios from "axios";
 import { Box } from "../../component/atoms/box";
 import { PokemonCard } from "../../component/moleculas/pokemonCard";
-import { Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, Stack, TextField } from "@mui/material";
-import { toast } from 'react-toastify';
+import { Button, Stack, TextField } from "@mui/material";
+import { toast } from "react-toastify";
 import { ScrollToTopButton } from "../../component/moleculas/scrollToTop";
 
 interface Pokemon {
@@ -13,30 +13,26 @@ interface Pokemon {
 }
 
 export const Home: React.FC = () => {
-
   const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
-  const [chosenPokemon, setChosenPokemon] = useState('');
-  const [itensPerPage, setItensPerPage] = useState(''); // itens por paginas
-  const [searchPokemon, setSearchPokemon] = useState(''); // armazena pokemon filtrado
-
-  const handleChange = (event: SelectChangeEvent) => {
-    setItensPerPage(event.target.value as string);
-  };
+  const [chosenPokemon, setChosenPokemon] = useState("");
+  const [itensPerPage, setItensPerPage] = useState(20); // itens por paginas
+  const [searchPokemon, setSearchPokemon] = useState(""); // armazena pokemon filtrado
 
   const cleanFilter = () => {
-    setChosenPokemon('');
-    setItensPerPage('');
-    setSearchPokemon('');
+    setChosenPokemon("");
+    setSearchPokemon("");
   };
 
   const catchPokemon = async () => {
     try {
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${searchPokemon}`);
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${searchPokemon}`
+      );
       const paginationData = response.data;
       const { name } = paginationData;
       setChosenPokemon(name);
     } catch (error) {
-      toast.error('Esse Pokemon não existe, ou você confudiu com Digimon');
+      toast.error("Esse Pokemon não existe, ou você confudiu com Digimon");
     }
   };
 
@@ -44,14 +40,24 @@ export const Home: React.FC = () => {
     // Função assíncrona para fazer a requisição da API
     const fetchPokemon = async () => {
       try {
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${itensPerPage}`);
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/?limit=${itensPerPage}`
+        );
         const paginationData = response.data;
         const { results } = paginationData;
         setPokemonList(results);
       } catch (error) {
-        console.error('Erro ao buscar o Pokémon:', error);
+        console.error("Erro ao buscar o Pokémon:", error);
       }
     };
+
+    window.addEventListener("scroll", function () {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        // console.log("Você rolou até o final da página!");
+        const addMorePokemons = itensPerPage + 20;
+        setItensPerPage(addMorePokemons);
+      }
+    });
 
     // Chama a função para fazer a requisição da API assim que o componente é montado
     fetchPokemon();
@@ -69,45 +75,34 @@ export const Home: React.FC = () => {
         position="relative"
         flexDirection="column"
       >
-        <Box display="flex" flexDirection="row" justifyContent="center" mt="20px" width="100%">
-          <Box
-            display="flex"
-            flexDirection="row"
-            marginX="10px"
-          >
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="center"
+          mt="20px"
+          width="100%"
+        >
+          <Box display="flex" flexDirection="row" marginX="10px">
             <Stack direction="row" spacing={2}>
               <TextField
                 label="Procurar Pokemon"
                 variant="outlined"
                 value={searchPokemon}
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                  setSearchPokemon(event.currentTarget.value);
+                  setSearchPokemon(event.currentTarget.value.toLowerCase());
                 }}
               />
-              <Button variant="outlined" onClick={catchPokemon} >Buscar</Button>  
-              <Button variant="outlined" onClick={cleanFilter} color="error">Limpar</Button>
+              <Button variant="outlined" onClick={catchPokemon}>
+                Buscar
+              </Button>
+              <Button variant="outlined" onClick={cleanFilter} color="error">
+                Limpar
+              </Button>
             </Stack>
-          </Box>
-          <Box minWidth="160px">
-            <FormControl fullWidth>
-              <InputLabel id="quantidade-label">Quantidade</InputLabel>
-              <Select
-                labelId="quantidade-label"
-                id="quantidade-label-select"
-                value={itensPerPage}
-                label="Quantidade"
-                onChange={handleChange}
-              >
-                <MenuItem value={"?limit=20"}>20 por página</MenuItem>
-                <MenuItem value={"?limit=40"}>40 por página</MenuItem>
-                <MenuItem value={"?limit=60"}>60 por página</MenuItem>
-                <MenuItem value={"?limit=80"}>80 por página</MenuItem>
-              </Select>
-            </FormControl>
           </Box>
         </Box>
 
-        <Box 
+        <Box
           display="flex"
           alignItems="flex-start"
           justifyContent="center"
@@ -116,7 +111,10 @@ export const Home: React.FC = () => {
           minHeight="100vh"
           marginY="50px"
         >
-          <PokemonCard pokemonList={pokemonList} chosenPokemon={chosenPokemon} />
+          <PokemonCard
+            pokemonList={pokemonList}
+            chosenPokemon={chosenPokemon}
+          />
         </Box>
 
         <ScrollToTopButton />
